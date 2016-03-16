f2a9d785b04aab37474f6ceadcc0387bb146db40
Alexander Cuellar Morales <alexcuellar@live.com>
2016-03-16 15:10:57 -0500

    [ADD] migrations for data export

---
+++ database/migrations/2016_03_16_194619_create_taxes_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('taxes', function (Blueprint $table) {
+            $table->integer('cta_cte_renta_id');
+            $table->integer('tributo');
+            $table->integer('ano_aplicacion');
+            $table->date('fecha_generacion');
+            $table->double('base_imponible', 18, 3);
+            $table->char('estado_proceso', 1);
+            $table->double('insoluto', 18, 3);
+            $table->char('generado', 1);
+            $table->integer('propietario_id');
+            $table->integer('predio_expediente_id');
+            $table->integer('predio_id');
+            $table->char('siglas', 3);
+            $table->char('derecho_emision_grupo_id',1);
+            $table->char('derecho_emision_grupo_desc', 60);
+            $table->integer('cuota');
+            $table->integer('numero_recibo_emision');
+            $table->double('importe_cuota', 15, 3);
+            $table->double('importe_emision', 15, 3);
+            $table->date('fecha_vencimiento');
+        });
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('taxes');
+    }
+}
---
+++ database/migrations/2016_03_16_200457_create_persons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('persons', function (Blueprint $table) {
+            $table->integer('persona_municipio_id');
+            $table->char('tipo_doc', 2);
+            $table->char('num_doc', 20);
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno');
+            $table->string('nombre');
+            $table->string('datos_completo');
+            $table->string('direccion');
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('persons');
+    }
+}
